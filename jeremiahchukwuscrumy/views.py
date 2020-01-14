4037be2350c0227fd5416d857d6e806ef4d2812a
Jayco <jeremiahchukwu@gmail.com>
2020-01-14 15:12:52 +0100

    view update

--- jeremiahchukwuscrumy/views.py
+++ jeremiahchukwuscrumy/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to Django")
+    return HttpResponse("This is a Scrum Application")
 
