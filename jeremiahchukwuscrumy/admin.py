e6e08a5d4f829ef7a5b41d8bfc0eda9cdc6e6481
Jayco <jeremiahchukwu@gmail.com>
2020-01-15 21:34:19 +0100

    models update

--- jeremiahchukwuscrumy/admin.py
+++ jeremiahchukwuscrumy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import GoalStatus, ScrumyGoals, ScrumyHistory
 
 # Register your models here.
+admin.site.register (GoalStatus)
+admin.site.register (ScrumyGoals)
+admin.site.register (ScrumyHistory)
--- jeremiahchukwuscrumy/models.py
+++ jeremiahchukwuscrumy/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.status_name
+
+class ScrumyGoals(models.Model):
+    goal_name =  models.CharField(max_length=100)
+    goal_id =  models.IntegerField()
+    created_by =  models.CharField(max_length=100)
+    moved_by =  models.CharField(max_length=100)
+    owner =  models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='jeremiahchukwuscrumy')
+    goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT, related_name='jeremiahchukwuscrumy')
+
+    def __str__(self):
+        return u'%s %s %s %s %s %s %s' % (self.goal_name, self.goal_id, self.created_by, self.moved_by, self.owner, self.user, self.goal_status)
+
+class ScrumyHistory(models.Model):
+    goal = models.ForeignKey(ScrumyGoals, on_delete=models.PROTECT, related_name='jeremiahchukwuscrumy')
+    moved_by =  models.CharField(max_length=100)
+    created_by =  models.CharField(max_length=100)
+    moved_from =  models.CharField(max_length=100)
+    moved_to =  models.CharField(max_length=100)
+    time_of_action =  models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.created_by
