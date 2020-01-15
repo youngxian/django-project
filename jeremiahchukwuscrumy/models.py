4edb9109c5f3d4089eee9616c36d29903f1fe8ad
Jayco <jeremiahchukwu@gmail.com>
2020-01-15 22:26:15 +0100

    goals

--- jeremiahchukwuscrumy/models.py
+++ jeremiahchukwuscrumy/models.py
@@ -9,11 +9,11 @@ class GoalStatus(models.Model):
         return self.status_name
 
 class ScrumyGoals(models.Model):
-    goal_name =  models.CharField(max_length=100)
-    goal_id =  models.IntegerField()
-    created_by =  models.CharField(max_length=100)
-    moved_by =  models.CharField(max_length=100)
-    owner =  models.CharField(max_length=100)
+    goal_name = models.CharField(max_length=100)
+    goal_id = models.IntegerField()
+    created_by = models.CharField(max_length=100)
+    moved_by = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='jeremiahchukwuscrumy')
     goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT, related_name='jeremiahchukwuscrumy')
 
@@ -22,11 +22,11 @@ class ScrumyGoals(models.Model):
 
 class ScrumyHistory(models.Model):
     goal = models.ForeignKey(ScrumyGoals, on_delete=models.PROTECT, related_name='jeremiahchukwuscrumy')
-    moved_by =  models.CharField(max_length=100)
-    created_by =  models.CharField(max_length=100)
-    moved_from =  models.CharField(max_length=100)
-    moved_to =  models.CharField(max_length=100)
-    time_of_action =  models.CharField(max_length=100)
+    moved_by = models.CharField(max_length=100)
+    created_by = models.CharField(max_length=100)
+    moved_from = models.CharField(max_length=100)
+    moved_to = models.CharField(max_length=100)
+    time_of_action = models.CharField(max_length=100)
 
     def __str__(self):
         return self.created_by
