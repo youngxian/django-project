2242f1d68a1d59896a78ccb073024f1013b8141d
Jayco <jeremiahchukwu@gmail.com>
2020-02-07 00:58:02 +0100

    solve

--- websocketscrumy/models.py
+++ websocketscrumy/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 
-
 # Create your models here.
 
 # Create your models here.
 class Connection(models.Model):
-    connection_id = models.CharField(max_length=255, default="")
+    connection_id = models.CharField(max_length=255)
+
     def __str__(self):
         return self.connection_id
 
 
-
 class ChatMessage(models.Model):
-    username = models.CharField(max_length=50, default="")
-    messages = models.CharField(max_length=400, default="")
-    timestamp = models.CharField(max_length=100, default="")
+    username = models.CharField(max_length=50)
+    messages = models.CharField(max_length=400)
+    timestamp = models.CharField(max_length=100)
+
     def __str__(self):
         return self.messages
