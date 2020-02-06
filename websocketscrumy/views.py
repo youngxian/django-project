6d5444575eff157117790e870aea6294966c3fb5
Jayco <jeremiahchukwu@gmail.com>
2020-02-06 23:32:07 +0100

    loool

--- websocketscrumy/views.py
+++ websocketscrumy/views.py
@@ -36,7 +36,7 @@ def disconnect(request):
 
 @csrf_exempt
 def send_message(request):
-    body = request.body
+    body = _parse_body(request.body)
     savemessage = ChatMessage(username=body["username"], messages=body["message"], timestamp=body["timestamp"])
     savemessage.save()
     connections = Connection.objects.all()
