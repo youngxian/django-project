75535d99a20f84092103b066a3620acfe72354c4
Jayco <jeremiahchukwu@gmail.com>
2020-02-06 23:01:37 +0100

    url update

--- websocketscrumy/urls.py
+++ websocketscrumy/urls.py
@@ -9,5 +9,6 @@ app_name = 'websocketscrumy'
 urlpatterns = [
     path('test/', views.test, name='test'),
     path('connect/', views.connect, name='connect'),
-    path('disconnect/', views.disconnect, name='disconnect')
+    path('disconnect/', views.disconnect, name='disconnect'),
+    path('send_message/', views.send_message, name='send_message')
 ]
--- websocketscrumy/views.py
+++ websocketscrumy/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import Connection, ChatMessage
+import boto3
 
 # Create your views here.
 
@@ -21,7 +22,6 @@ def  _parse_body(body):
 @csrf_exempt
 def connect(request):
     body = _parse_body(request.body)
-    #remember to save it using the connectionmodel task 28
     connection_id = body['connectionId']
     connect = Connection(connection_id=connection_id)
     connect.save()
@@ -33,3 +33,19 @@ def disconnect(request):
     connection_id = body['connectionId']
     connect = Connection.objects.get(connection_id=connection_id)
     return JsonResponse({'message': 'disconnect successfully'}, status=200)
+
+def send_message(request):
+    body = _parse_body(request.body)
+    savemessage = ChatMessage(username=body['username'], messages=body['message'], timestamp=body['timestamp'])
+    savemessage.save()
+    connections = Connection.objects.all()
+    data = {'messages': body}
+    for eachconnect in connections:
+        _send_to_connection(eachconnect['connection_id'], data)
+    return JsonResponse({'message': 'successfully send'}, status=200)
+
+
+
+def _send_to_connection(connection_id, data):
+    gatewayapi = boto3.client("apigatewaymanagementapi", endpoint_url='https://0l90clyplf.execute-api.us-east-2.amazonaws.com/test/@connections', region_name='us-east-2', aws_access_key_id='AKIAJLUF2B3ATOW2HXVA', aws_secret_access_key=' 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')
+    return gatewayapi.post_to_connection(ConnectionId=connection_id, Data=json.dumps(data).encode('utf-8'))
