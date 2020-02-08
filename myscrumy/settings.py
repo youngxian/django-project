dada2dc3233de2d73349ad80b70348de0fcd57c3
Jayco <jeremiahchukwu@gmail.com>
2020-02-08 15:28:12 +0100

    update

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -126,6 +126,3 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/jeremiahchukwuscrumy/home'
 
-AWS_ACCESS_KEY_ID = 'AKIAYKJFHT2IEJHLAVU5'
-
-AWS_SECRET_ACCESS_KEY = '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'
