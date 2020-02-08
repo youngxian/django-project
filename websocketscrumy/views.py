7a353fba49e510d4399e38d47d4165bc0a575f00
Jayco <jeremiahchukwu@gmail.com>
2020-02-08 19:22:36 +0100

    update

--- websocketscrumy/views.py
+++ websocketscrumy/views.py
@@ -50,12 +50,7 @@ def send_message(request):
     savemessage.save()
     connections = Connection.objects.filter()
     allconnect = list(connections.values('connection_id'))
-    message = {
-        "message": message,
-        "username": username,
-        "timestamp": timestamp
-    }
-    data = {'messages': [list(message)]}
+    data = {'messages': [list(allconnect)]}
     for eachconnect in allconnect:
         conn = eachconnect['connection_id']
         _send_to_connection(str(conn), data)
@@ -64,19 +59,8 @@ def send_message(request):
 
 @csrf_exempt
 def _send_to_connection(connection_id, data):
-    # auth = AWSRequestsAuth(aws_access_key='AKIAYKJFHT2IEJHLAVU5',
-    #                        aws_secret_access_key='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',
-    #                        aws_host='https://0l90clyplf.execute-api.us-east-2.amazonaws.com/test/@connections',
-    #                        aws_region='us-east-2',
-    #                        aws_service='execute-api')
-    # response = requests.post('https://0l90clyplf.execute-api.us-east-2.amazonaws.com/test/@connections', json=json.dumps(data),
-    #                         auth=auth)
-    # print("test-", response.content)
-    
     gatewayapi = boto3.client('apigatewaymanagementapi', region_name='us-east-2', endpoint_url='https://0l90clyplf.execute-api.us-east-2.amazonaws.com/test/',
                               aws_access_key_id=os.getenv('ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('SECRET_KEY'), aws_session_token=os.getenv('SESSION_TOKEN'))
-    
-    # Data = json.dumps(data).encode('utf-8')
     return gatewayapi.post_to_connection(ConnectionId=connection_id, Data=json.dumps(data).encode('utf-8'))
 
 
@@ -86,7 +70,6 @@ def getRecentMessages(request):
     connection_id = body['connectionId']
     allmessage = ChatMessage.objects.filter()
     result_list = list(allmessage.values('username', 'messages', 'timestamp'))
-    result_list.reverse()
     data = {'messages': result_list}
     # print("print - ", json.dumps(data))
     return JsonResponse({'message': json.dumps(data)}, status=200)
