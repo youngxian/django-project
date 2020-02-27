888390550e60a4168bc2c6823ab85456446a64fa
Jayco <jeremiahchukwu@gmail.com>
2020-02-27 22:59:55 +0100

    changes

--- websocketscrumy/views.py
+++ websocketscrumy/views.py
@@ -51,7 +51,7 @@ def send_message(request):
     savemessage.save()
     messages = {
         "username":username,
-        "message":message,
+        "content":message,
         "timestamp":timestamp
     }
     connections = Connection.objects.filter()
